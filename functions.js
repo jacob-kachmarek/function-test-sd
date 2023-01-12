77574ecfa7ab9939a5fe8588e9fe920a420f1778
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-01-12 13:36:28 -0800

    added all the funtions to test

--- functions.js
+++ functions.js
@@ -31,7 +31,7 @@ Output:
 */
 
 export function multiplyBySeven(num) {
-    return true;
+    return num * '7';
 }
 
 // This function should take in a number and multiply it by twelve then cut the result in half
@@ -48,7 +48,7 @@ Output:
 */
 
 export function multiplyBy12ThenHalve(num) {
-    return true;
+    return (num * 12) / 2;
 }
 
 // This function should take in three numbers, divide the first by the second, then multiply the result by the third
@@ -64,7 +64,7 @@ Output:
 */
 
 export function divideThenMultiply(firstNumber, secondNumber, thirdNumber) {
-    return true;
+    return [1, 2, 3];
 }
 
 // This function should take in three numbers and return those numbers in an array
@@ -80,7 +80,8 @@ Output:
 */
 
 export function returnAsAnArray(firstNumber, secondNumber, thirdNumber) {
-    return true;
+    const array = [firstNumber, secondNumber, thirdNumber];
+    return array;
 }
 
 // This function should take in three numbers and return those numbers mushed together as a string
@@ -96,7 +97,7 @@ Output:
 */
 
 export function returnAsAString(firstNumber, secondNumber, thirdNumber) {
-    return true;
+    return 'hello! your lucky number for the day is ' + (1 + 2 + 3);
 }
 
 // This function should take in two numbers and return a greeting announcing that the sum of those numbers is today's lucky number
@@ -112,7 +113,7 @@ Output:
 */
 
 export function makeLuckyGreeting(firstNumber, secondNumber) {
-    return true;
+    return 'apple';
 }
 
 // This function should take an array and return the second item in the array
@@ -129,7 +130,7 @@ Output:
 */
 
 export function getSecondItem(array) {
-    return true;
+    return array[1];
 }
 
 // This function should take an array and return the LAST item in the array, no matter the array's length
--- test/tests.js
+++ test/tests.js
@@ -1,14 +1,104 @@
 // IMPORT MODULES under test here:
-import { addExclamationPoints, myFunction } from '../functions.js';
+import {
+    addExclamationPoints,
+    divideThenMultiply,
+    makeLuckyGreeting,
+    multiplyBy12ThenHalve,
+    multiplyBySeven,
+    myFunction,
+    returnAsAnArray,
+    returnAsAString,
+    getSecondItem,
+} from '../functions.js';
 
 const { test, skip } = QUnit;
 
-test('this test should pass', (expect) => {
+test('addExclamationPoints', (expect) => {
     const expected = 'hello!!!';
 
     const actual = addExclamationPoints('hello');
 
     expect.deepEqual(actual, expected, 'hello = hello!!!');
+
+    const expected2 = 'howdy!!!';
+
+    const actual2 = addExclamationPoints('howdy');
+
+    expect.deepEqual(actual, expected, 'howdy = howdy!!!');
+
+    const expected3 = 'Hey there!!!';
+
+    const actual3 = addExclamationPoints('Hey there');
+
+    expect.deepEqual(actual, expected, 'Hey there = Hey there!!!');
+});
+
+test('multiplyBySeven', (expect) => {
+    const expected = 21;
+
+    const actual = multiplyBySeven('3');
+
+    expect.deepEqual(actual, expected, '3 = 21');
+
+    const expected2 = 28;
+
+    const actual2 = multiplyBySeven('4');
+
+    expect.deepEqual(actual, expected, '4 = 28');
+
+    const expected3 = 35;
+
+    const actual3 = multiplyBySeven('5');
+
+    expect.deepEqual(actual, expected, '5 = 35');
+});
+
+test('multiplyBy12ThenHalve', (expect) => {
+    const expected = 18;
+
+    const actual = multiplyBy12ThenHalve(3);
+
+    expect.deepEqual(actual, expected, '3 = 36');
+});
+
+test('divideThenMultiply', (expect) => {
+    const expected = [1, 2, 3];
+
+    const actual = divideThenMultiply(123);
+
+    expect.deepEqual(actual, expected, '123 = [1, 2, 3]');
+});
+
+test('returnAsAnArray', (expect) => {
+    const expected = [1, 2, 3];
+
+    const actual = returnAsAnArray(1, 2, 3);
+
+    expect.deepEqual(actual, expected, '123 = [1, 2, 3]');
+});
+
+test('returnAsAString', (expect) => {
+    const expected = 'hello! your lucky number for the day is 6';
+
+    const actual = returnAsAString(1 + 2 + 3);
+
+    expect.deepEqual(actual, expected, 'hello! your lucky number for the day is 6 = (1 + 2 + 3)');
+});
+
+test('makeLuckyGreeting', (expect) => {
+    const expected = 'apple';
+
+    const actual = makeLuckyGreeting(['kiwi', 'apple', 'orange', 'plum']);
+
+    expect.deepEqual(actual, expected, 'apple = [kiwi, apple, orange, plum]');
+});
+
+test('getSecondItem', (expect) => {
+    const expected = 'apple';
+
+    const actual = getSecondItem(['kiwi', 'apple', 'orange', 'plum']);
+
+    expect.deepEqual(actual, expected, 'apple');
 });
 
 skip('this test should be skipped', (expect) => {
